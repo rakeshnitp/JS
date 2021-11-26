2a08e5a51f322188f8ecd085da21c02fba936731
rakeshnitp <91244047+rakeshnitp@users.noreply.github.com>
2021-11-26 21:12:35 +0530

    Add files via upload

---
+++ qn1.js
@@ -0,0 +1,29 @@
+var Person1 = new Object()
+Person1.FirstName = "Rakesh",
+    Person1.LastName = "Kumar",
+    Person1.OfficeAddress = "Patna,Bihar"
+
+var Person2 = new Object()
+Person2.FirstName = "Abhimanyu",
+    Person2.LastName = "Anand",
+    Person2.OfficeAddress = "Mumbai,Maharashtra"
+
+var Person3 = new Object()
+Person3.FirstName = "Roshan",
+    Person3.LastName = "Raj",
+    Person3.OfficeAddress = "Saharsa,Bihar"
+
+console.log("Person1:");
+console.log("FirstName: " + Person1.FirstName);
+console.log("LastName: " + Person1.LastName);
+console.log("OfficeAddress: " + Person1.OfficeAddress);
+
+console.log("Person2:");
+console.log("FirstName: " + Person2.FirstName);
+console.log("LastName: " + Person2.LastName);
+console.log("OfficeAddress: " + Person2.OfficeAddress);
+
+console.log("Person3:");
+console.log("FirstName: " + Person3.FirstName);
+console.log("LastName: " + Person3.LastName);
+console.log("OfficeAddress: " + Person3.OfficeAddress);
